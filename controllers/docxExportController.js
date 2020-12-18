5c0189a157c1f9cb1df132ab344fccb0ab07fe2d
Lotte <lotteunckell@gmail.com>
2020-12-18 18:47:39 +0100

    add node version for heroku

--- controllers/docxExportController.js
+++ controllers/docxExportController.js
@@ -2,7 +2,6 @@
 const docx_exporter = require('../docx-exporter/docx-export')
 
 exports.program = (req, res) => {
-  //res.send('working on it')
   const program_data = req.body
   const code = program_data.code ? program_data.code.split(' ').join('') : 'XX'
   const name = program_data.code ? program_data.name.split(' ').join('') : 'xxx'
@@ -12,8 +11,7 @@ exports.program = (req, res) => {
   docx_exporter.export(filename, program_data)
   res.download(filename, function(err){
     if (err) {
-      // if the file download fails, we throw an error
-      throw err;
+      throw err
     }
   })
 }
