4c1c9b27e8b11a755c7a9846d5af5c60e4403242
Lotte <lotteunckell@gmail.com>
2020-12-18 19:11:00 +0100

    add dynamic port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030
 //const exporter = require('./exporter/docx-export')
 const helmet = require('helmet')
 const router = require('./router')
@@ -10,7 +10,7 @@ const router = require('./router')
 app.use(helmet())
 
 app.listen(port, () => {
-  console.log(`For local develompent: exporter listening at http://localhost:${port}`)
+  console.log(`exporter listening at http://localhost:${port}`)
 })
 
 app.use('/', router)
