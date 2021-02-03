1af93e0b255972bf2637dbba15622e34fa4363fe
JuliaZamaitat <j.zamaitat@gmail.com>
2021-02-03 18:27:50 +0100

    working for hardcoded sose21, needs replacement for imported file from rails.

--- excel_importer/excel_import.js
+++ excel_importer/excel_import.js
@@ -2,10 +2,29 @@ class ExcelImport {
   doSomething() {
     const planungSoSe21 = {
       sheetName: 'Gesamt',
-      headerRange: 'A1:W1',
-      valueRange: 'A2:W90',
       xlsx: 'Lehrplanung_01.02.2021.xls'
     };
+
+    const { xlsx2ModuleList } = require('./xlsxScanner');
+    const { htw2uas, groupModules, extractAndMap } = require('./schemaConverter');
+
+    const sose21 = xlsx2ModuleList(planungSoSe21);
+    const prettifiedSoSe21 = sose21.map((m) => htw2uas(m)).reduce(groupModules, {});
+
+    const fieldMapping = {
+      'Sem. von': 'semester',
+      'B / M': 'bm',
+      name: 'name',
+      teacher: 'teacher',
+      Modulverantwortlicher: 'responsible'
+    };
+
+    const listSoSe21 = extractAndMap(prettifiedSoSe21, fieldMapping);
+
+    for (const code in listSoSe21) {
+      const m = listSoSe21[code];
+      console.log([m.bm, code, m.name, `${m.teacher.firstName} ${m.teacher.lastName}`, m.responsible].join(', '));
+    }
   }
 }
 
---
+++ excel_importer/schemaConverter.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// https://medium.com/javascript-in-plain-english/how-to-deep-copy-objects-and-arrays-in-javascript-7c911359b089
+//import _ from "lodash" // Import the entire lodash library
+// import { clone, cloneDeep } from "lodash" // Alternatively: Import just the clone methods from lodash
+
+const _ = require('lodash');
+const codeNameRegex = /\s*(\D{1,4}\d\d?(\.\d)?)(.+)/;
+
+module.exports.htw2uas = function (moduleHTW) {
+  const uas = _.cloneDeep(moduleHTW);
+  const codeName = moduleHTW['LV Name'];
+  const codeNameArray = codeName?.match(codeNameRegex);
+  if (typeof codeNameArray === 'undefined' || codeNameArray == null || codeNameArray.length < 3) {
+    console.error(`could not parse ${codeName}`);
+  } else {
+    uas.code = codeNameArray[1];
+    uas.name = codeNameArray[3];
+    delete uas['LV Name'];
+  }
+
+  uas.teacher = {
+    lastName: moduleHTW.Dozentname,
+    firstName: moduleHTW.Dozentvorname,
+    role: moduleHTW.Status
+  };
+  delete uas['Dozentname'];
+  delete uas['Dozentvorname'];
+  delete uas['Status'];
+
+  uas.semester = moduleHTW['Sem. von'];
+
+  return uas;
+};
+
+module.exports.groupModules = function (modules, module) {
+  const { code } = module;
+  if (!(code in modules)) {
+    modules[code] = { code: code, modules: [] };
+    modules[code] = { modules: [] };
+  }
+  modules[code].modules.push(module);
+  return modules;
+};
+
+module.exports.extractAndMap = function (allModules, fieldMap) {
+  const result = {};
+  for (const code in allModules) {
+    const list = allModules[code].modules;
+    const fieldsForModule = {};
+    for (const sourceField in fieldMap) {
+      const targetField = fieldMap[sourceField];
+      const module = list.find((m) => {
+        return !(m[sourceField] === undefined);
+      });
+      if (module === undefined) {
+        console.log(`Module ${code}:no field with name ${sourceField}`);
+      } else {
+        fieldsForModule[targetField] = module[sourceField];
+      }
+    }
+    result[code] = fieldsForModule;
+  }
+  return result;
+};
